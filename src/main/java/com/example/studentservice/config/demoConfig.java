89e103f3d84373d3ebb52153c3e23e20fe17e209
KalyanReddy605 <kalyanreddy@gmail.com>
2025-12-01 16:56:56 +0530

    add java spring boot

--- src/main/java/com/example/studentservice/config/demoConfig.java
+++ src/main/java/com/example/studentservice/config/demoConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Configurable;
 public class demoConfig {
 	
 	public String hello() {
-		return "Hello world ";
+		return "Hello world java spring boot";
 	}
 
 }
